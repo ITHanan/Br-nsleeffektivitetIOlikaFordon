8962d652822033d1ef7688c41588eeacabc4e00c
ITHanan <155847905+ITHanan@users.noreply.github.com>
2024-09-26 10:44:56 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿namespace BränsleeffektivitetIOlikaFordon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Skapa en Console App som jämför bränsleeffektivitet i olika fordon. Vi måste använda 2 OOP Principer
+
+            Console.WriteLine("Enter the amount of fuel consumed in liters ");
+            string bränsleLiterSomString = Console.ReadLine()!;
+            int bränsleLiter = Convert.ToInt32(bränsleLiterSomString);
+
+            Console.WriteLine("Enter distans in KM");
+            string distansSomString = Console.ReadLine()!;
+            int KM = Convert.ToInt32(distansSomString);
+
+           Vehicle ferrari = new Vehicle();
+            ferrari.fuelconsumeing = bränsleLiter;
+            ferrari.distancetraveling= KM;
+
+            double eficientcy = ferrari.Calculatbränsleeffektivitet();
+            Console.WriteLine($"bränsleFörbeukningen är :{eficientcy}");
+
+            Console.WriteLine("Enter the amount of fuel consumed in liters ");
+            string bränsleLiterLastbilSomString = Console.ReadLine()!;
+            int bränsleLiterlastbil = Convert.ToInt32(bränsleLiterLastbilSomString);
+
+            Console.WriteLine("Enter distans in KM");
+            string distansSomStringlastbil = Console.ReadLine()!;
+            int LastbilKM = Convert.ToInt32(distansSomStringlastbil);
+
+            Lastbil scania = new Lastbil();
+            scania.fuelconsumeing = bränsleLiterlastbil;
+            scania.distancetraveling = LastbilKM;
+
+            scania.bränsle = "diesel";
+
+
+            double eficientcyscania = scania.Calculatbränsleeffektivitet();
+            Console.WriteLine($"bränsleFörbeukningen är :{eficientcyscania}");
+
+        }
+    }
+}
---
+++ Vehicle.cs
@@ -0,0 +1,49 @@
+﻿
+
+namespace BränsleeffektivitetIOlikaFordon
+{
+    public  class Vehicle
+    {
+        public int fuelconsumeing;
+        public int distancetraveling;
+
+        public virtual double Calculatbränsleeffektivitet()
+        { 
+            return fuelconsumeing / distancetraveling;
+        }
+    }
+
+
+    public class Lastbil : Vehicle
+    {
+        public string bränsle;
+        public override double Calculatbränsleeffektivitet()
+
+        {
+            int specialsiffra;
+
+            if (bränsle == "diesel")
+            {
+                specialsiffra = 2;
+            }
+
+            else
+            {
+                specialsiffra = 5;
+            };
+            return fuelconsumeing / distancetraveling / specialsiffra;
+
+          
+
+        }
+
+    }
+
+    public class Car : Vehicle
+    {
+        public string  modell;
+       
+
+    }
+    
+}
